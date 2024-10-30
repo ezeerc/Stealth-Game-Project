4ae1707128798b5e4046576e949c66e8187d5dcb
TomasRacciatti <tomasracciatti@gmail.com>
2024-10-30 19:04:18 -0300

    Rotacion de camara smooth

--- Assets/Scripts/Camera/CameraRotator.cs
+++ Assets/Scripts/Camera/CameraRotator.cs
@@ -7,22 +7,43 @@ public class CameraRotator : MonoBehaviour
 
     private float _currentAngle = -45;
     private float _isometricAngle = 30;
+    private bool _isRotating = false;
+
+    [SerializeField] float rotationSpeed = 5f;
     
     public void RotateRight()
     {
-        _currentAngle -= 90f;
-        RotateCamera();
+        if (!_isRotating)
+        {
+            _currentAngle -= 90f;
+            StartCoroutine(SmoothRotate());
+        }
     }
 
     public void RotateLeft()
     {
-        _currentAngle += 90f;
-        RotateCamera();
+        if (!_isRotating)
+        {
+            _currentAngle += 90f;
+            StartCoroutine(SmoothRotate());
+        }
     }
 
-    private void RotateCamera()
+    private IEnumerator SmoothRotate()
     {
-        Quaternion newRotation = Quaternion.Euler(_isometricAngle, _currentAngle, 0);
-        transform.rotation = newRotation;
+        _isRotating = true;
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.Euler(_isometricAngle, _currentAngle, 0);
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime * rotationSpeed;
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, t);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+        _isRotating = false;
     }
 }
