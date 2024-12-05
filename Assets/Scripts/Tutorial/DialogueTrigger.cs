f1ec9f3942fe449b1706dd9d64e34dceccefba0d
TomasRacciatti <tomasracciatti@gmail.com>
2024-12-05 15:14:53 -0300

    Inicio de prompts

---
+++ Assets/Scripts/Tutorial/DialogueTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public DialogueData dialogueData;
+    [SerializeField] private LayerMask playerLayerMask;
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasTriggered || (playerLayerMask.value & (1 << other.gameObject.layer)) == 0) return;
+
+        hasTriggered = true;
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogueData);
+    }
+}
