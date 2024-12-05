6c101038d638d4f3e52cbe3e9d50dc79ef3e1e51
TomasRacciatti <tomasracciatti@gmail.com>
2024-12-05 16:14:51 -0300

    Los triggers activan el canvas progresivamente

--- Assets/Scripts/Tutorial/DialogueTrigger.cs
+++ Assets/Scripts/Tutorial/DialogueTrigger.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    [SerializeField] private DialogueData dialogueData;
-    [SerializeField] private LayerMask playerLayerMask;
-    private bool hasTriggered = false;
+    [SerializeField] private DialogueData _dialogueData;
+    [SerializeField] private LayerMask _playerLayerMask;
+    [SerializeField] private List<GameObject> _canvasObjects;
+    private bool _hasTriggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (hasTriggered || (playerLayerMask.value & (1 << other.gameObject.layer)) == 0) return;
+        if (_hasTriggered || (_playerLayerMask.value & (1 << other.gameObject.layer)) == 0) return;
 
-        hasTriggered = true;
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogueData);
+        _hasTriggered = true;
+
+        foreach (GameObject canvasItem in _canvasObjects)
+        {
+            if (canvasItem != null)
+            {
+                canvasItem.SetActive(true);
+            }
+        }
+
+        FindObjectOfType<DialogueManager>().StartDialogue(_dialogueData);
     }
 }
