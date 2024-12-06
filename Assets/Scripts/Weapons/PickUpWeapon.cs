a38d9d59368bfb82342873a709494c61fd29c602
TomasRacciatti <tomasracciatti@gmail.com>
2024-12-06 11:40:59 -0300

    sistema de eventos para los triggers para activar el pick up del arma

--- Assets/Scripts/Weapons/PickUpWeapon.cs
+++ Assets/Scripts/Weapons/PickUpWeapon.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 
 public class PickUpWeapon : MonoBehaviour
 {
-    [SerializeField, Tooltip("number 0 - Shotgun; number 1 - Uzi; number 2 - AK47; number 3 - M5; number 4 - Sniper; number 5 - pistol; number 6 - nothing")] 
+    [SerializeField, Tooltip("number 0 - Shotgun; number 1 - Uzi; number 2 - AK47; number 3 - M5; number 4 - Sniper; number 5 - pistol; number 6 - nothing")]
     private int number;
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.layer == 6)
         {
-                var player = other.gameObject.GetComponent<Player>();
-                player.gameObject.GetComponent<WeaponController>().ChangeWeapon(number);
-                Destroy(this.gameObject);
+            PickUp();
         }
+    }
+
+    public void PickUp()
+    {
+        var player = FindAnyObjectByType<Player>();
+        player.gameObject.GetComponent<WeaponController>().ChangeWeapon(number);
+        Destroy(this.gameObject);
     }
 }
