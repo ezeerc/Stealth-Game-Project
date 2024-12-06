d61f54d0fb7f622a8dfc64814f8a2abb8c3107ca
DESKTOP-H5KP010\Ezequiel <ezeerc2212@gmail.com>
2024-12-05 23:33:22 -0300

    Agregado pick up pistola.-

--- Assets/Scripts/Weapons/PickUpWeapon.cs
+++ Assets/Scripts/Weapons/PickUpWeapon.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 
 public class PickUpWeapon : MonoBehaviour
 {
+    [SerializeField, Tooltip("number 0 - Shotgun; number 1 - Uzi; number 2 - AK47; number 3 - M5; number 4 - Sniper; number 5 - pistol; number 6 - nothing")] 
+    private int number;
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
                 var player = other.gameObject.GetComponent<Player>();
-                player.gameObject.GetComponent<WeaponController>().ChangeWeapon(1);
+                player.gameObject.GetComponent<WeaponController>().ChangeWeapon(number);
                 Destroy(this.gameObject);
         }
     }
